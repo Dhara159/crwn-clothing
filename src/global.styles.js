03f514dac5752f3f72f94b20d564776e2d448c4e
Dhara Vihol <viholdhara@gmail.com>
2020-02-22 22:05:56 +0530

    Resolved the issue of createGlobalStyle

--- src/global.styles.js
+++ src/global.styles.js
@@ -1,6 +1,6 @@
-import { CreateGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = CreateGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Open Sans Condensed';
     padding: 20px 40px;
